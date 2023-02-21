5e53c13708f9e67a5873171f230a67b6cce507dc
CullenAddie <cullenaddie1@gmail.com>
2023-02-21 22:57:19 +0000

    Cullen A

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -43,7 +43,13 @@ public class Lab {
         String buildingString = "";
 
 //        code here
+        for (int i = start; i< end; i++) {
+            buildingString = buildingString + i + " ";
+        }
+                return buildingString;
+         }}
+            
 
-        return buildingString;
-    }
-}
+        
+            
+        
